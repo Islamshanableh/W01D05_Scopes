b50c8710b90e3415aee9a6d342e5576297fe5dd2
Islamshanableh <islam.alshanableh@gmail.com>
2021-07-09 01:43:47 +0300

    P try Q6

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 console.log(arrowTitle)
 /* START CODE UNDER THIS LINE */
 
+// pulse Check
+// Q1 
+
+let myFavoriteFood = "burger"
+const favoriteFood = function () {
+    return myFavoriteFood;
+};
+
+// Q2 why myFavoriteFood didnt update
+
+const updateFavoriteFood = function (newValue) {
+  myFavoriteFood = newValue ;
+  return myFavoriteFood ;
+
+};
+
+
+// Q3
+
+const createCounter = function () {
+ let counter = 0; //i change this to comment to solve Q4 
+
+  // this anonymous function is a closure function that has access to its own closure variable
+  return function () {
+    return ++counter;
+  };
+};
+const counter1 =createCounter()
+const counter2 = createCounter()
+
+// Q4
+const createCounter1 = function (start) {
+  counter = start;
+  return "count is update"
+};
+
+
+
+
+
+
+
+
+
+
 // Practice
 // Q1
 let age = 25;
@@ -44,7 +89,7 @@ const func2 = function (age) {
 
 
 // Q3 
-let num= 4;
+let num= 5;
 const countDown= function(){
     num = num - 1
     if (num < 0){
@@ -55,3 +100,31 @@ const countDown= function(){
         return num;
     }
 }
+
+// Q4 
+
+const countUp= function(){
+    num = num + 1
+    return num
+}
+
+
+// Q5 
+
+const restCount=function (start){
+       num = start ;
+       return "the count has been reset"
+       
+}
+
+// Q6 
+
+const addToList = function (toDo) {
+   
+   return toDo 
+};
+
+
+
+
+
